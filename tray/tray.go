c6e0b4e46f23454df373a665b2529cf6af866a54
CrossR <r.cross@lancaster.ac.uk>
2022-04-01 23:27:43 +0100

    Persist the application state between runs.

--- tray/tray.go
+++ tray/tray.go
@@ -1,13 +1,15 @@
 package tray
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/CrossR/kb_ui/tray/icons"
 
-	"github.com/getlantern/systray"
-
+	"github.com/adrg/xdg"
 	"github.com/gen2brain/beeep"
+	"github.com/getlantern/systray"
 	"golang.design/x/hotkey"
 )
 
@@ -24,6 +26,11 @@ type TrayState struct {
 	keybinds   *[]Keybinding
 	layer_id   int
 	layer_name string
+}
+
+type SaveState struct {
+	PreviousId   int    `json:"id"`
+	PreviousName string `json:"name"`
 }
 
 func Start() {
@@ -47,16 +54,34 @@ func trayEnd(state *TrayState) {
 		hk.bind = nil
 	}
 
-	// TODO: Save the current state of the application to load on next run.
+	dataFile, err := xdg.DataFile("kb_ui/state.json")
+	if err != nil {
+		return
+	}
+
+	endState := SaveState{state.layer_id, state.layer_name}
+	json, err := json.MarshalIndent(endState, "", "    ")
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(dataFile, json, 0644)
+
+	if err != nil {
+		return
+	}
+
 }
 
 // On ready, load the user configuration, setup the keybindings, then just wait
 // and react to them.
 func traySetup(state *TrayState) {
 
+	// Setup the default parts of the system tray.
 	systray.SetTemplateIcon(icons.KB_Dark_Data, icons.KB_Dark_Data)
 	systray.SetTooltip("Keyboard Status")
 
+	// Load the user config, but stop if there is nothing defined.
 	config, err := LoadConfiguration()
 
 	if len(config.LayerInfo) == 0 {
@@ -64,6 +89,8 @@ func traySetup(state *TrayState) {
 		return
 	}
 
+	// Set a default layer, so there is something set before checking for a
+	// previous run file.
 	defaultName := fmt.Sprintf("%s Layer", config.LayerInfo[0].Name)
 	mCurrentLayer := systray.AddMenuItem(defaultName, defaultName)
 
@@ -72,10 +99,27 @@ func traySetup(state *TrayState) {
 		return
 	}
 
+	// Add the final entries to configure or quit the application.
+	systray.AddSeparator()
+	mConfigure := systray.AddMenuItem("Configure", "Configure")
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
+	// Load the previous run file, if it exists.
+	// This can be used to setup the default layer.
+	// Don't worry about errors, just ignore it since its only a save state of
+	// the previous state.
+	dataFile, err := xdg.DataFile("kb_ui/state.json")
+	if err != nil {
+		return
+	}
+	file, _ := ioutil.ReadFile(dataFile)
+
+	prevState := SaveState{}
+	json.Unmarshal(file, &prevState)
 
 	for i, binding := range config.LayerInfo {
 
+		// Parse the configurations strings into its mods / keys and icon.
 		mods := ParseModifiers(binding.Mods)
 		if len(mods) == 0 {
 			systray.Quit()
@@ -95,25 +139,39 @@ func traySetup(state *TrayState) {
 		}
 
 		keybind := Keybinding{nil, mods, key, i, binding.Name, &icon}
-		setupKeybinding(&keybind, mCurrentLayer)
+		setupKeybinding(state, &keybind, mCurrentLayer)
 
 		if keybind.bind == nil {
 			continue
 		}
 
+		// Store the binding, so we can unregister it later.
 		*state.keybinds = append(*state.keybinds, keybind)
+
+		// If this is the state we left off in last time, set it.
+		if binding.Name == prevState.PreviousName {
+			mCurrentLayer.SetTitle(fmt.Sprintf("%s Layer", keybind.name))
+			systray.SetIcon(*keybind.icon)
+		}
 	}
 
+	// On tray quit event.
 	go func() {
 		<-mQuit.ClickedCh
 		systray.Quit()
 	}()
+
+	// On configure event.
+	go func() {
+		<-mConfigure.ClickedCh
+		OpenConfig()
+	}()
 }
 
 // Setup the actual keybinds to notify the user of layer changes.
-func setupKeybinding(bind *Keybinding, trayItem *systray.MenuItem) {
+func setupKeybinding(state *TrayState, keybind *Keybinding, trayItem *systray.MenuItem) {
 
-	hk := hotkey.New(bind.mods, bind.key)
+	hk := hotkey.New(keybind.mods, keybind.key)
 	err := hk.Register()
 
 	if err != nil {
@@ -121,14 +179,21 @@ func setupKeybinding(bind *Keybinding, trayItem *systray.MenuItem) {
 	}
 
 	go func() {
-		layerSwapName := fmt.Sprintf("Swapped to %s layer", bind.name)
+		layerSwapName := fmt.Sprintf("Swapped to %s layer", keybind.name)
 		for hk != nil {
 			<-hk.Keydown()
+			// Notify the user of the layer change.
 			beeep.Notify("Layer Swapped", layerSwapName, "")
-			trayItem.SetTitle(fmt.Sprintf("%s Layer", bind.name))
-			systray.SetIcon(*bind.icon)
+
+			// Update the tray icon and title.
+			trayItem.SetTitle(fmt.Sprintf("%s Layer", keybind.name))
+			systray.SetIcon(*keybind.icon)
+
+			// Make sure the app state is saved.
+			state.layer_id = keybind.id
+			state.layer_name = keybind.name
 		}
 	}()
 
-	bind.bind = hk
+	keybind.bind = hk
 }
