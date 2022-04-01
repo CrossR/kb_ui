5b9a1a0698445c4ecdc9bb115ba35cd580517a49
CrossR <r.cross@lancaster.ac.uk>
2022-04-01 13:56:22 +0100

    Add basics for tracking keybinds.
    Store the created binds, add ids and names.

--- tray/tray.go
+++ tray/tray.go
@@ -10,45 +10,84 @@ import (
 	"golang.design/x/hotkey"
 )
 
+type Keybinding struct {
+	bind *hotkey.Hotkey
+	mods []hotkey.Modifier
+	key  hotkey.Key
+	id   int
+	name string
+}
+
+type TrayState struct {
+	keybinds   *[]Keybinding
+	layer_id   int
+	layer_name string
+}
+
 func Start() {
+	var keybinds []Keybinding
+	trayState := TrayState{&keybinds, 0, ""}
+
+	onReady := func() {
+		traySetup(&trayState)
+	}
+	onExit := func() {
+		trayEnd(&trayState)
+	}
+
 	systray.Run(onReady, onExit)
 }
 
 // On exit, save the current state of the application, un-register any keybindings.
-func onExit() {
+func trayEnd(state *TrayState) {
+	for _, hk := range *state.keybinds {
+		hk.bind.Unregister()
+		hk.bind = nil
+	}
 }
 
 // On ready, load the user configuration, setup the keybindings, then just wait
 // and react to them.
-func onReady() {
+func traySetup(state *TrayState) {
 	systray.SetTemplateIcon(icon.Data, icon.Data)
 	systray.SetTooltip("Keyboard Status")
 
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 
-        setupKeybinding([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
-	setupKeybinding([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyT)
+	keys := []hotkey.Key{hotkey.KeyS, hotkey.KeyT}
 
-	go func() {
-		<-mQuit.ClickedCh
-		fmt.Println("Requesting quit")
-		systray.Quit()
-		fmt.Println("Finished quitting")
-	}()
-}
+	for _, key := range keys {
+		hk, ok := setupKeybinding([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, key)
 
-func setupKeybinding(mods []hotkey.Modifier, key hotkey.Key) {
+		if !ok {
+			continue
+		}
 
-	hk := hotkey.New(mods, key)
-	err := hk.Register()
-	if err != nil {
-		return
+		*state.keybinds = append(*state.keybinds, hk)
 	}
 
 	go func() {
-		for {
+		<-mQuit.ClickedCh
+		systray.Quit()
+	}()
+}
+
+func setupKeybinding(mods []hotkey.Modifier, key hotkey.Key) (bind Keybinding, ok bool) {
+
+	hk := hotkey.New(mods, key)
+	err := hk.Register()
+
+	if err != nil {
+		return Keybinding{}, false
+	}
+
+	go func() {
+		hotKeyPressed := fmt.Sprintf("%s was pressed", hk.String())
+		for hk != nil {
 			<-hk.Keydown()
-			beeep.Notify("Pressed", "Down", "")
+			beeep.Notify("Pressed", hotKeyPressed, "")
 		}
 	}()
+
+	return Keybinding{hk, mods, key, 0, ""}, true
 }
